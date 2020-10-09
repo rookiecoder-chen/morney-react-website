4c7388cb292437e40dc72eab8ce1a4a2c1ebd6bf
chenjie <rokiecoder@163.com>
2020-10-09 22:23:32 +0800

    init

---
+++ precache-manifest.ed045cf972165f440040e3ddd1153104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045c8b92f3160283e5701f45a08f0230",
+    "url": "/morney-react-website/index.html"
+  },
+  {
+    "revision": "3dfc6f606d9b27a779a2",
+    "url": "/morney-react-website/static/css/main.2eb8b012.chunk.css"
+  },
+  {
+    "revision": "1b1562b2dde7cc8fec74",
+    "url": "/morney-react-website/static/js/2.473c0537.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/morney-react-website/static/js/2.473c0537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dfc6f606d9b27a779a2",
+    "url": "/morney-react-website/static/js/main.06d7c5e6.chunk.js"
+  },
+  {
+    "revision": "c11a6a8a76c7e9b2f676",
+    "url": "/morney-react-website/static/js/runtime-main.1456168a.js"
+  }
+]);
